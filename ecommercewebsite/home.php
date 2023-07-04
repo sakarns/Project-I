1433869fa3a73cc85e9bb50fd0ef629975d7755a
sakarns <mainalisakar222@gmail.com>
2023-07-04 14:00:48 +0545

    1.0.4

--- ecommercewebsite/home.php
+++ ecommercewebsite/home.php
@@ -196,11 +196,6 @@ include 'components/wishlist_cart.php';
 
 
 
-
-
-
-
-
    <?php include 'components/footer.php'; ?>
 
    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
