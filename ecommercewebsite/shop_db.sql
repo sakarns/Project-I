b6b9cbfe92b30df30cc72136a3b2355b78a527f7
sakarns <mainalisakar222@gmail.com>
2023-07-13 17:51:31 +0545

    1.0.7

--- ecommercewebsite/shop_db.sql
+++ ecommercewebsite/shop_db.sql
@@ -1,10 +1,17 @@
 -- phpMyAdmin SQL Dump
+
 -- version 5.1.1
+
 -- https://www.phpmyadmin.net/
+
 --
+
 -- Host: 127.0.0.1
+
 -- Generation Time: Jul 1, 2023 at 09:59 PM
+
 -- Server version: 10.4.22-MariaDB
+
 -- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
