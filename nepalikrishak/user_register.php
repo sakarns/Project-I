06a71616501c3a7fcd24854469843e69cce6cfb2
Sakar Mainali <135095231+sakarns@users.noreply.github.com>
2024-12-02 10:10:41 +0545

    Update user_register.php
    remove pInfo

--- nepalikrishak/user_register.php
+++ nepalikrishak/user_register.php
@@ -59,15 +59,15 @@ if (isset($_POST['submit'])) {
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'mainalisakar222@gmail.com';             // SMTP username
+            $mail->Username   = 'example@gmail.com';             // SMTP username
             $mail->Password   = 'xtjioswjjqxvdauc';                      // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // Enable implicit TLS encryption
             $mail->Port       = 465;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             // Recipients
-            $mail->setFrom('mainalisakar222@gmail.com', 'Sakar');
+            $mail->setFrom('example@gmail.com', 'uname');
             $mail->addAddress($email, $name);     // Add a recipient
-            $mail->addReplyTo('mainalisakar222@gmail.com', 'Sakar');
+            $mail->addReplyTo('example222@gmail.com', 'Sakar');
 
             // Content
             $mail->isHTML(true);                                  // Set email format to HTML
